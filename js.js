57cba30ee076cf3aa51f445a073936d1ce645f5f
Ivan Tabeev <qatabeev@gmail.com>
2023-04-04 00:58:20 +0400

    initial commit

---
+++ js.js
@@ -0,0 +1,12 @@
+let QA = 'Ivan';
+let age = 32;
+let status = 'open to work';
+
+console.log('Hello, LinkedIn!');
+console.log("I'm " + QA + ', ' + age + ' years old.');
+
+if (status === 'open to work') {
+  console.log("If I find a new job, I'll be happy.");
+} else {
+  console.log('I found a new job! Congratulate me!');
+}; 
